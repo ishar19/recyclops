1c49295f8b29a91985c4f370dd3e1c4631a7119e
ishar <isharjain19@gmail.com>
2023-01-14 01:55:45 +0530

    init:frontend setup with vite:react+swc, tailwind

---
+++ frontend/recyclops/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="flex justify-center items-center h-[100vh] bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+      <p className="text-7xl tracking-widest  text-center font-extrabold text-[#facc15] animate-bounce">Recyclops</p>
+    </div>
+  )
+}
+
+export default App
