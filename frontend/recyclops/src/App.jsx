a3df0c2b6261f5055f3ea9979b2dcc5035ce7060
ishar <isharjain19@gmail.com>
2023-01-15 16:07:40 +0530

    chore:added tailwind prettier and babel

--- frontend/recyclops/src/App.jsx
+++ frontend/recyclops/src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function App() {
   return (
-    <div className="flex h-[100vh] items-center justify-center bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-      <p className="animate-bounce  text-center text-7xl  font-extrabold tracking-widest text-[#facc15]">
+    <div className=" flex h-[100vh] items-center justify-center bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+      <p className=" animate-bounce text-center text-7xl font-extrabold tracking-widest text-[#facc15]">
         Recyclops
       </p>
       <p className="flex font-extralight tracking-widest"></p>
