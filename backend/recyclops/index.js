633862344d47636a4bbaa66d587df864f51286aa
ishar <isharjain19@gmail.com>
2023-02-11 06:47:49 +0530

    feat:added express, nodemon, CORS

---
+++ backend/recyclops/index.js
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv' 
+dotenv.config()
+import express from 'express'
+import cors from 'cors'
+var app = express()
+const PORT = process.env.PORT
+app.use(cors())
+ 
+app.get('/', function (req, res, next) {
+  res.send("RECYCLOPS")
+})
+ 
+app.listen(PORT, function () {
+  console.log(`running on ${PORT}`)
+})
