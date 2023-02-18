fcad0f5f5b25831f8f8fae06d8fd7207caa66c46
ishar <isharjain19@gmail.com>
2023-02-19 00:44:29 +0530

    feat: new scan done and deployed

--- backend/recyclops/routes/scan.js
+++ backend/recyclops/routes/scan.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 import firebaseApp from '../firebaseConfig.js'
-import {arrayRemove, arrayUnion, doc ,getDoc, getFirestore,setDoc,Timestamp } from "firebase/firestore";
+import {arrayRemove, arrayUnion, doc ,getDoc, getFirestore,setDoc,Timestamp, updateDoc,addDoc, collection } from "firebase/firestore";
 const db = getFirestore(firebaseApp);
 import getImage from '../utils/getImage.js';
 import uploadImage from '../utils/uploadImage.js';
@@ -12,13 +12,32 @@ router.use((req, res, next) => {
 })
 
 router.post('/newScan',async(req, res)=>{
-    const {dataURI} = req.body
-    const file = await getImage(dataURI)
-    const publicURL = await uploadImage(file)
-    const response = await fetch(`https://fastapi-production-0f75.up.railway.app/classify_image?image_url=${publicURL}`);
-    const data = await response.json();
-    console.log(data);
-    res.send(publicURL)
+    const {dataURI,userId=null} = req.body
+    let publicURL;
+    let scanClass;
+    let scanId;
+    try{
+        const file = await getImage(dataURI)
+        publicURL = await uploadImage(file)
+        const response = await fetch(`https://fastapi-production-0f75.up.railway.app/classify_image?image_url=${publicURL}`);
+        const data = await response.json()
+        scanClass = data['class_integer'].toString()
+        const scanDataRef = doc(db, "scanData",data['class_integer'].toString())
+        const scanSnap = await getDoc(scanDataRef)
+        const scanInfo = scanSnap.data()
+        const newScanRef = await addDoc(collection(db,"scans"),{
+            publicURL:publicURL,
+            scanClass:scanClass,
+            userId:userId,
+            createdAt:Timestamp.fromDate(new Date(Date.now()))
+        })
+        scanId = newScanRef.id
+        res.send({scanInfo, scanId})
+    }
+    catch(e){
+        console.log(e)
+        res.sendStatus(500)
+    }
 })
 
 export default router
--- backend/recyclops/utils/uploadImage.js
+++ backend/recyclops/utils/uploadImage.js
@@ -1,8 +1,8 @@
 import firebaseApp from "../firebaseConfig.js";
 import { getStorage, ref,uploadBytes, uploadString } from "firebase/storage";
-import {readFileSync} from 'fs'
+import {readFileSync,unlink} from 'fs'
 import admin from 'firebase-admin'
-import serviceAccount from '../recyclops-69781-firebase-adminsdk-tqt5x-5f8d35fdbc.json' assert { type: "json" };
+import serviceAccount from '../recyclops-69781-firebase-adminsdk-tqt5x-5f8d35fdbc.json'assert { type: "json" };
 const storage = getStorage(firebaseApp);
 import { v4 as uuidv4 } from 'uuid';
 
@@ -30,7 +30,11 @@ const uploadImage = async(filePath,dataURI)=>{
     metadata: metadata,
   }).then((res)=>{return {downloadToken:res[0]['metadata']['metadata']["firebaseStorageDownloadTokens"],name:res[0]['metadata']['name']}}).catch((e)=>{console.log(e)})
   const URL = downloadUrl(process.env.FIREBASE_STORAGE_BUCKET,name,downloadToken)
+  unlink(filePath,()=>{
+    console.log("file deleted",filePath)
+  })
   return URL
+
 
 }
 
