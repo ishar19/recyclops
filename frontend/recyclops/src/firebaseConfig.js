25cdf000d2c43fdf208629e2f3927f3a777b3d05
ishar <isharjain19@gmail.com>
2023-02-18 22:27:35 +0530

    fix: firebase api key error

--- frontend/recyclops/src/firebaseConfig.js
+++ frontend/recyclops/src/firebaseConfig.js
@@ -1,8 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+console.log(import.meta.VITE_AUTH_DOMAIN);
 const firebaseConfig = {
-  apiKey: import.meta.VITE_API_KEY,
   authDomain: import.meta.VITE_AUTH_DOMAIN,
+  apiKey: import.meta.VITE_API_KEY,
   projectId: import.meta.VITE_PROJECT_ID,
   storageBucket: import.meta.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.VITE_MESSAGING_SENDER_ID,
--- frontend/recyclops/vite.config.js
+++ frontend/recyclops/vite.config.js
@@ -5,7 +5,7 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { https: true },
+  //   server: { https: true },
 
   plugins: [react(), eslint(), mkcert()],
 });
