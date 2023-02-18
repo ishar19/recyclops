2907f24f793bbaa0ed817433da9d94ae38a3a283
ishar <isharjain19@gmail.com>
2023-02-18 15:25:09 +0530

    feat:added railway model link

--- backend/recyclops/routes/scan.js
+++ backend/recyclops/routes/scan.js
@@ -5,6 +5,7 @@ import {arrayRemove, arrayUnion, doc ,getDoc, getFirestore,setDoc,Timestamp } fr
 const db = getFirestore(firebaseApp);
 import getImage from '../utils/getImage.js';
 import uploadImage from '../utils/uploadImage.js';
+import fetch from 'node-fetch';
 router.use((req, res, next) => {
   console.log('Time: ', Date.now())
   next()
@@ -14,6 +15,9 @@ router.post('/newScan',async(req, res)=>{
     const {dataURI} = req.body
     const file = await getImage(dataURI)
     const publicURL = await uploadImage(file)
+    const response = await fetch(`https://fastapi-production-0f75.up.railway.app/classify_image?image_url=${publicURL}`);
+    const data = await response.json();
+    console.log(data);
     res.send(publicURL)
 })
 
