6bc950c964ad53551205c4ccd7bcb701fe2ea362
ishar <isharjain19@gmail.com>
2023-02-18 22:48:18 +0530

    fix: solved env problem

--- frontend/recyclops/src/firebaseConfig.js
+++ frontend/recyclops/src/firebaseConfig.js
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-console.log(import.meta.VITE_AUTH_DOMAIN);
 const firebaseConfig = {
-  authDomain: import.meta.VITE_AUTH_DOMAIN,
-  apiKey: import.meta.VITE_API_KEY,
-  projectId: import.meta.VITE_PROJECT_ID,
-  storageBucket: import.meta.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.VITE_APP_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
