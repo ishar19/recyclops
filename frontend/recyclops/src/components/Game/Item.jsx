6966a77148f10630fdbb43842ece8fc0fc857fb9
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2023-02-24 18:21:37 +0530

    game: item fixed

--- frontend/recyclops/src/components/Game/Item.jsx
+++ frontend/recyclops/src/components/Game/Item.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Item = () => {
   const item = "item";
   return (
-    <div className="absolute top-[43gvh]  flex h-[32vh] w-[100vw] items-center justify-center bg-[#C2BB75]">
+    <div className="absolute bottom-[25vh] flex h-[32vh] w-[100vw] items-center justify-center bg-[#C2BB75]">
       <div className="rounded-full bg-[#D9D9D9] p-10">{item}</div>
     </div>
   );
