64af0f8809e47defeabcc1be6d7151f7c0f8ceed
ishar <isharjain19@gmail.com>
2023-02-25 03:31:17 +0530

    feat:save scan api added

--- frontend/recyclops/src/APIs/User.js
+++ frontend/recyclops/src/APIs/User.js
@@ -11,3 +11,31 @@ export const addScan = async (userId, scanId) => {
     body: JSON.stringify(body),
   }).then(async (data) => await data.json());
 };
+
+export const saveScan = async (userId, scanId) => {
+  const body = {
+    userId: userId,
+    scanId: scanId,
+  };
+  return fetch(`http://localhost:5000/user/saveScan`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  }).then(async (data) => data.ok);
+};
+
+export const removeScan = async (userId, scanId) => {
+  const body = {
+    userId: userId,
+    scanId: scanId,
+  };
+  return fetch(`http://localhost:5000/user/saveScan`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  }).then(async (data) => data.ok);
+};
--- frontend/recyclops/src/components/Scanner/Info.jsx
+++ frontend/recyclops/src/components/Scanner/Info.jsx
@@ -5,12 +5,12 @@ import Footer from "./Footer";
 import PropTypes from "prop-types";
 
 const Info = ({ data }) => {
-  const { scanInfo } = data;
+  const { scanInfo, scanId } = data;
   return (
     <div className="flex flex-col items-center">
       <Category category={scanInfo.category} color={scanInfo.color} />
       <Dispose category={scanInfo.class} info={scanInfo.info} />
-      <Footer />
+      <Footer scanId={scanId} />
     </div>
   );
 };
