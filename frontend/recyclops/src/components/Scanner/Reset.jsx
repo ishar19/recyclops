8d43790f616d87ffed1413fb7d1064d89e24ef2f
ishar19 <lordcero7@gmail.com>
2023-02-26 21:15:13 +0530

    fix: logo import typo

--- frontend/recyclops/src/components/Scanner/Reset.jsx
+++ frontend/recyclops/src/components/Scanner/Reset.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 // import staticLoaderLogo from "../../assets/staticLoaderLogo.svg";
-import staticLoaderLogo from "../../assets/Recycleops.gif";
+import Loader from "../../Assets/Recycleops.gif";
 const Reset = () => {
   return (
     <div className="absolute z-[-2]">
       <img
-        src={staticLoaderLogo}
+        src={Loader}
         className=" relative top-[50vh] left-[50vw] h-[50vh] translate-x-[-50%] translate-y-[-50%]"
       />
     </div>
