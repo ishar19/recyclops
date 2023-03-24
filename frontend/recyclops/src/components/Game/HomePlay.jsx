d1eb3adcc8e77b3532a24cbce6d1417a952da803
ishar <isharjain19@gmail.com>
2023-03-24 12:43:41 +0530

    feat: added loading in game

--- frontend/recyclops/src/components/Game/HomePlay.jsx
+++ frontend/recyclops/src/components/Game/HomePlay.jsx
@@ -3,15 +3,14 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import dragAndDrop from "../../Assets/Drag and drop.png";
 import { newGame } from "../../APIs/Game";
-
 // eslint-disable-next-line react/prop-types
-const HomePlay = ({ user }) => {
-  const [loading, setLoading] = useState(false);
+const HomePlay = ({ user, setLoading }) => {
   const navigate = useNavigate();
   const navigateToGame = async () => {
     setLoading(true);
     if (user != null)
       await createNewGame().then(() => {
+        setLoading(false);
         navigate("/game/play");
       });
     else {
@@ -25,10 +24,7 @@ const HomePlay = ({ user }) => {
     window.localStorage.setItem("gameData", JSON.stringify(gameData));
   };
   return (
-    <button
-      className={`mx-8 ${loading ? "animate-pulse" : ""} relative`}
-      onClick={navigateToGame}
-    >
+    <button className={`relative mx-8`} onClick={navigateToGame}>
       {/* <span className="absolute top-1 right-2  inline-flex h-[5vh] w-[5vh] animate-ping rounded-full bg-red-500"></span> */}
       <img src={dragAndDrop} alt="Play Game" />
     </button>
